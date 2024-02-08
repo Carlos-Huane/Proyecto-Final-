0ac835e6682f7284f3dc8ff7a4c875f96478d942
Carlos-Huane <carloshuanesarmiento@gmail.com>
2024-02-07 23:39:25 -0500

    vista_general

---
+++ mavenproject1/src/main/java/Vista/Vista_General.java
@@ -0,0 +1,42 @@
+package Vista;
+import javax.swing.JOptionPane;
+import Vista.Admin.Menu_Admin;
+import Vista.Empleado.Empleado;
+public class Vista_General {
+    public static void main (String[] args){
+        Menu_Admin menu_admin = new Menu_Admin();
+        Empleado menu_empleado= new Vista.Empleado.Empleado();
+        int opcion;
+        String menu = """
+                ############################
+                              Bienvenido
+                ############################
+                1. INICIAR COMO ADMINISTRADOR
+                2. INICIAR COMO EMPLEADO
+                3. Salir
+        """;
+        do {
+        String input = JOptionPane.showInputDialog(menu);
+        
+        if (input == null) {
+            opcion = 3;     // Si se presiona "Cancelar", establece la opción a 5
+        } else {
+            opcion = Integer.parseInt(input);
+        }
+        switch (opcion) {
+            case 1 -> {
+                menu_admin.mostrar_menu_admin();
+            }
+            case 2 -> {
+                menu_empleado.mostrar_menu_empleado();
+            }
+            case 3 -> {
+                break;
+            }
+            default -> {
+                JOptionPane.showMessageDialog(null, "Coloque un número entre 1-3");
+            }
+        }
+        }while (opcion!=3);
+    }
+}
